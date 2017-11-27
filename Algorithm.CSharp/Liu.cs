268b0983c4ac001ccb55b2280e9adedc82bb7431
Joshua Gou <joshuagou@astri.org>
2017-11-27 16:09:01 +0800

    --amend

---
+++ Algorithm.CSharp/Liu.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Globalization;
+using QuantConnect.Data;
+using QuantConnect.Data.Market;
+using QuantConnect.Statistics;
+using QuantConnect.Indicators;
+using QuantConnect.Orders;
+
+namespace QuantConnect.Algorithm.CSharp
+{
+    public class Liu : QCAlgorithm
+    {
+        string symbol = "601166-SSE";
+        string bar;
+        int barNumber = 0;
+        int entryBar = 0;
+        int exitBar = 0;
+        decimal tolerance = 0.1m; //0.1% safety margin in prices to avoid bouncing
+        DateTime sampledToday = DateTime.Now;
+        decimal TPPrice = 0;
+        decimal SLPrice = 0;
+        decimal lastPrice = 0;
+        decimal lastBuyPrice = 0;
+
+        // position related
+        int holdings = 0;
+        int quantity = 0;
+        decimal exposure = 0m;
+        decimal openPrice, highPrice, lowPrice, closePrice;
+        decimal nav = 10000000m;
+        OrderTicket orderTicket;
+
+        // indicators
+        RelativeStrengthIndex rsi;
+        MovingAverageConvergenceDivergence macd;
+
+        // initialize the data and resolution you require for your strategy:
+        public override void Initialize()
+        {
+            SetStartDate(2016, 01, 01);
+            SetEndDate(2016, 02, 28);  // DateTime.Now
+            SetCash(nav);
+
+            // add security
+            AddData<AstriData>(symbol, Resolution.Minute, TimeZones.Shanghai, Market.SSE, false, 1m);  // Tick, Second, Minute, Hour, Daily
+            // indicators
+            rsi = RSI(symbol, 14, MovingAverageType.Wilders, Resolution.Hour);
+            macd = MACD(symbol, 12, 26, 9, MovingAverageType.Exponential, Resolution.Hour);
+        }
+
+        //Handle TradeBar Events: a TradeBar occurs on every time-interval
+        public void OnData(AstriData data)
+        {
+            //if (data.Time == Convert.ToDateTime("2015-05-05 14:13:00", CultureInfo.InvariantCulture) || data.Time == Convert.ToDateTime("2015-04-23 10:30:00", CultureInfo.InvariantCulture))
+            //    Console.WriteLine(data.Time.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture));
+            // live mode
+            if (LiveMode)
+                SetRuntimeStatistic(symbol, data.Close.ToString("C"));
+
+            // the opening time of the current bar
+            bar = data.Time.ToString();
+            barNumber += 1;
+            openPrice = data.Open;
+            highPrice = data.High;
+            lowPrice = data.Low;
+            closePrice = data.Close;
+
+            // print OHLC for each bar
+            // Log(bar + "," + openPrice + "," + highPrice + "," + lowPrice + "," + closePrice + "," + rsi);
+
+            // get first bar for the day and report
+            nav = Portfolio[symbol].HoldingsValue + Portfolio.Cash;
+            if (sampledToday.Date != data.Time.Date) Log(bar + ". Current NAV: " + nav);
+            sampledToday = data.Time;
+
+            holdings = Convert.ToInt32(Portfolio[symbol].Quantity);
+            // no position, and after a 5-min interval
+            if (holdings == 0 && barNumber - entryBar >= 60)
+            {
+                // buy if condition met
+                if (closePrice > lastPrice)
+                {
+                    entryBar = barNumber;
+                    // determine buy size
+                    decimal rawQuantity = nav / closePrice;
+                    // ensure multiples of 100
+                    quantity = Convert.ToInt32(Math.Floor(rawQuantity / 100)) * 100;
+
+                    // place buy order
+                    orderTicket = Order(symbol, quantity);
+                    Log("Buy " + quantity + " shares of " + symbol + " at " + closePrice);
+                    exposure = quantity * closePrice / nav; // trade-based exposure
+                    Log(bar + ". Quantity: " + quantity + ", Exposure: " + exposure);
+
+                    lastBuyPrice = closePrice;
+                    // set take profit and stop loss levels
+                    TPPrice = closePrice * 1.10m;
+                    SLPrice = closePrice * 0.90m;
+                    Log("Stop loss/take profit levels at (" + SLPrice + "," + TPPrice + ")");
+                }
+            }
+            // position exists, and after a 5-min interval
+            else if (holdings > 0 && barNumber - entryBar >= 60)
+            {
+                Boolean TPSL = highPrice >= TPPrice || lowPrice <= SLPrice;
+                // sell if condition met, or TP/SL met
+                if (closePrice > lastBuyPrice)
+                {
+                    entryBar = barNumber;
+                    // place sell order
+                    orderTicket = Order(symbol, -holdings);
+                    Log("Sell " + holdings + " shares of " + symbol + " at " + closePrice);
+                    Log(bar + ". Quantity: 0, Exposure: 0");
+                }
+            }
+
+
+            lastPrice = closePrice;
+        }
+
+        // end of day reporting
+        public override void OnEndOfDay()
+        {
+            // latest asset value
+            nav = Portfolio[symbol].HoldingsValue + Portfolio.Cash;
+        }
+
+        // monitor event arrival
+        public override void OnOrderEvent(OrderEvent orderEvent)
+        {
+            // Log(orderEvent.ToString());
+            // if order is filled
+            if (orderEvent.Status.ToString() == "Filled")
+            {
+                // MKT order filled at closing price
+                // Log("Filled at " + orderEvent.FillPrice);
+            }
+
+        }
+    }
+}
---
+++ Algorithm.CSharp/SampleAlgorithm3.cs
@@ -0,0 +1,128 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System;
+using QuantConnect.Data;
+using QuantConnect.Data.Market;
+using QuantConnect.Statistics;
+using QuantConnect.Indicators;
+using QuantConnect.Orders;
+using System.Collections.Generic;
+
+namespace QuantConnect.Algorithm.CSharp
+{
+    /*
+    *   Long Only
+    *   Minutely Data
+	*	End of Date Reporting
+    *	Equal Weighting Portfolio
+    * 
+    */
+    public class SampleAlgorithm3 : QCAlgorithm
+    {
+        // define required variables
+        // strategy related
+        List<string> symbols = new List<string>
+        {
+            "000002-SZSE",
+            "000060-SZSE",
+            "000156-SZSE",
+            "000157-SZSE"
+        };
+        string bar;
+        DateTime sampledToday = DateTime.Now;
+        decimal p = 0m;
+
+        // position related
+        int holdings = 0;
+        int quantity = 0;
+        decimal exposure = 0m;
+        decimal closePrice = 0m;
+        decimal nav = 1000000m;
+        OrderTicket orderTicket;
+
+        // indicators
+
+        // initialize the data and resolution you require for your strategy:
+        public override void Initialize()
+        {
+            SetStartDate(2015, 01, 01);
+            SetEndDate(2016, 02, 28);  // DateTime.Now
+            SetCash(nav);
+
+            p = 1/(decimal)symbols.Count;
+            foreach (var symbol in symbols)
+            {
+                // add security
+                AddData<AstriData>(symbol, Resolution.Minute, TimeZones.Shanghai, Market.SZSE, false, 1m);  // Tick, Second, Minute, Hour, Daily
+                // indicators
+            }
+        }
+
+        //Handle TradeBar Events: a TradeBar occurs on every time-interval
+        public void OnData(AstriData data)
+        {
+            // live mode
+            if (LiveMode)
+            {
+                foreach (var symbol in symbols)
+                {
+                    SetRuntimeStatistic(symbol, data.Close.ToString("C"));
+                }
+            }
+            
+            // the opening time of the current bar
+            bar = data.Time.ToString();
+            
+            // get first bar for the day and report
+            if (sampledToday.Date != data.Time.Date)
+            {
+                Log(bar + ". Current NAV: " + nav);
+                
+                foreach (var symbol in symbols)
+                {
+                    closePrice = Portfolio[symbol].Price;
+                    holdings = Convert.ToInt32(Portfolio[symbol].Quantity);
+                    decimal adj = nav * p / closePrice;
+                    quantity = Convert.ToInt32(Math.Floor(adj / 100)) * 100 - holdings;
+
+                    // place rebalancing order
+                    if (quantity != 0) 
+                    {
+                        orderTicket = Order(symbol, quantity);
+                        Log("Trade " + quantity + " shares of " + symbol + " at " + closePrice);
+                    }
+                    holdings += quantity;
+                    exposure = holdings * closePrice / nav; // trade-based exposure
+                    Log(bar + "." + symbol + ": Quantity: " + holdings + ", Exposure: " + exposure);
+                }
+            }
+            sampledToday = data.Time;
+        }
+
+        // end of day reporting
+        public override void OnEndOfDay()
+        {
+            nav = Portfolio.Cash;
+            foreach (var symbol in symbols)
+            {   
+                // latest asset value
+                nav += Portfolio[symbol].HoldingsValue;
+            }
+            
+        }
+        
+    }
+}
